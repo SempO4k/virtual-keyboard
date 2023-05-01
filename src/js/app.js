166e0841a2105ad508ea8d0572b6c4f1d7133bbc
Semp <tonito5551@gmail.com>
2023-05-01 17:30:19 +0300

    feat:fix: implement switch lang and fix situation when special symbols don't recognize

--- src/js/app.js
+++ src/js/app.js
@@ -7,6 +7,23 @@ const cssClasses = {
   TEXTAREA: 'textarea',
   KEYBOARD: 'keyboard',
 };
+const SPECIAL_KEYS = [
+  'Backspace',
+  'Delete',
+  'Space',
+  'Tab',
+  'Enter',
+  'CapsLock',
+  'ShiftLeft',
+  'ShiftRight',
+  'AltLeft',
+  'AltRight',
+  'ControlLeft',
+  'ControlRight',
+  'MetaLeft',
+];
+
+const pressed = new Set();
 
 function createElement(tagName, className) {
   const element = document.createElement(tagName);
@@ -46,69 +63,71 @@ window.addEventListener('load', () => {
   }
 });
 
-// document.querySelector('.keyboard').addEventListener('click', (event) => {
-//   console.log(event.target);
-//   const textarea = document.querySelector('.textarea');
-//   const point = textarea.selectionStart;
-//   console.log(point);
-// });
+function switchLang() {
+  const lang = document.querySelectorAll('.lang');
+  lang.forEach((key) => {
+    key.classList.contains('hidden')
+      ? key.classList.remove('hidden')
+      : key.classList.add('hidden');
+  });
+}
 
 document.addEventListener('keydown', (event) => {
   event.preventDefault();
   const textarea = document.querySelector('.textarea');
   const target = document.querySelector(`.${event.code}`);
   const point = textarea.selectionStart;
-  // const textarea = document.querySelector('textarea');
-  // textarea.focus();
-  // console.log(point);
+  pressed.add(event.code);
   // console.log(event.code);
   target.classList.add('active');
-  switch (event.code) {
-    case 'Space':
-      textarea.value += ' ';
-      break;
-    case 'CapsLock':
-      break;
-    case 'Enter':
-      textarea.value += '\n';
-      break;
-    case 'Backspace':
-      if (point > 0 && point <= textarea.value.length - 1) {
-        textarea.value = textarea.value.slice(0, point - 1)
-        + textarea.value.slice(point, textarea.value.length);
-        textarea.selectionStart = point - 1;
-        textarea.selectionEnd = point - 1;
-      } else {
-        textarea.value = textarea.value.slice(0, -1);
-        textarea.selectionStart = point - 1;
-        textarea.selectionEnd = point - 1;
-      }
-      break;
-    case 'Delete':
-      if (point >= 0 && point <= textarea.value.length - 1) {
-        textarea.value = textarea.value.slice(0, point)
-        + textarea.value.slice(point + 1, textarea.value.length);
-        textarea.selectionStart = point;
-        textarea.selectionEnd = point;
-      }
-      break;
-    case 'Tab':
-      textarea.value += '    ';
-      break;
-    default:
-      if (point >= 0 && point <= textarea.value.length) {
-        textarea.value = textarea.value.slice(0, point)
-        + target.outerText + textarea.value.slice(point, textarea.value.length);
-        textarea.selectionStart = point + target.outerText.length;
-        textarea.selectionEnd = point + target.outerText.length;
-      }
-      break;
+
+  if (SPECIAL_KEYS.includes(event.code)) {
+    switch (event.code) {
+      case 'Space':
+        textarea.value += ' ';
+        break;
+      case 'CapsLock':
+        break;
+      case 'Enter':
+        textarea.value += '\n';
+        break;
+      case 'Backspace':
+        if (point > 0 && point <= textarea.value.length - 1) {
+          textarea.value = textarea.value.slice(0, point - 1)
+          + textarea.value.slice(point, textarea.value.length);
+          textarea.selectionStart = point - 1;
+          textarea.selectionEnd = point - 1;
+        } else {
+          textarea.value = textarea.value.slice(0, -1);
+          textarea.selectionStart = point - 1;
+          textarea.selectionEnd = point - 1;
+        }
+        break;
+      case 'Delete':
+        if (point >= 0 && point <= textarea.value.length - 1) {
+          textarea.value = textarea.value.slice(0, point)
+          + textarea.value.slice(point + 1, textarea.value.length);
+          textarea.selectionStart = point;
+          textarea.selectionEnd = point;
+        }
+        break;
+      case 'Tab':
+        textarea.value += '\t';
+        break;
+      default:
+        break;
+    }
+    if (pressed.has('ShiftLeft') && pressed.has('AltLeft')) switchLang();
+  } else {
+    textarea.value = textarea.value.slice(0, point)
+    + target.outerText + textarea.value.slice(point, textarea.value.length);
+    textarea.selectionStart = point + target.outerText.length;
+    textarea.selectionEnd = point + target.outerText.length;
   }
 });
 
 document.addEventListener('keyup', (event) => {
   const target = document.querySelector(`.${event.code}`);
   target.classList.remove('active');
-  // console.log(target);
-  // console.log(event.code);
+  pressed.delete(event.code);
 });
